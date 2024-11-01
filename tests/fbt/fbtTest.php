05ba47c14907a6bd11d5227691985f31c01f193e
Richard Dobroň <dobronr@doorz.sk>
2024-11-01 16:18:53 +0100

    Supress error for older PHP versions

--- tests/fbt/fbtTest.php
+++ tests/fbt/fbtTest.php
@@ -833,9 +833,11 @@ FBT;
 
         $this->assertSame('By artist <span><a>Lou Reed</a> & <a>Metallica</a></span>', $fbt);
 
-        $this->expectException(FbtParserException::class);
+        if (version_compare(PHP_VERSION, '7.4.0') >= 0) {
+            $this->expectException(\Exception::class);
 
-        $fbt = (string)fbt('By artist ' . \fbt\fbt::param('artist', '<a>Lou Reed</a> & <a>Metallica</a>'), 'test');
+            (string)fbt('By artist ' . \fbt\fbt::param('artist', '<a>Lou Reed</a> & <a>Metallica</a>'), 'test');
+        }
     }
 
     public function testEmptyParameter()
