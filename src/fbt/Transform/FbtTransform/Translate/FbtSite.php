28fad58f53a8e386da53f9d59ebd2407c90b639a
richardDobron <richardDobron@users.noreply.github.com>
2026-07-13 18:18:41 +0000

    Fix styling

--- src/fbt/Transform/FbtTransform/Translate/FbtSite.php
+++ src/fbt/Transform/FbtTransform/Translate/FbtSite.php
@@ -115,7 +115,7 @@ class FbtSite
             $textToHash = [];
             foreach ($json['hashToText'] as $k => $text) {
                 invariant(
-                    !isset($textToHash[$text]), // undefined
+                    ! isset($textToHash[$text]), // undefined
                     "Duplicate texts pointing to different hashes shouldn't be possible"
                 );
                 $textToHash[$text] = $k;
